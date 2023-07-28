f6c9aa7bf712ad90c1e782fab57bb15473e0762d
oferei <ofer.eichenboim@mobuleye.com>
2023-07-28 17:04:11 +0300

    Modify main modules to work with uvicorn

--- InterviewExam/Controller/Controller.py
+++ InterviewExam/Controller/Controller.py
@@ -1,5 +1,6 @@
-import __init__
+#import __init__
 from DAL import DataAccessLayer as dal
+from Controller.InputConverter import convert_to_dict
 
 def empty_output_validation(tickets_list):
     if len(tickets_list)>0:
@@ -15,13 +16,17 @@ def get_all_tickets():
     tickets = dal.load_database()
     return output_gate(tickets)
     
-def tickets_text_filter(query_dict):
+def tickets_text_filter(query):
     '''This function returns all tickets that match text field-content from the list [(text field,searched content)]'''
+    query_dict = convert_to_dict(query)
+    print(query_dict)
+    print(type(query_dict))
     tickets = dal.filter_by_txt(query_dict)
     return output_gate(tickets)
 
-def ticket_time_filter(query_dict):
+def ticket_time_filter(query):
     '''This function returns all the tickets in the time frame'''
+    query_dict = convert_to_dict(query)
     tickets = dal.filter_by_time(query_dict)
     return output_gate(tickets)
 
