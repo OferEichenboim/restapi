e69679a6e0466fc0dae2bb61f6eafc5472620a64
oferei <ofer.eichenboim@mobuleye.com>
2023-07-28 17:20:33 +0300

    add Controller/inputconverter.py

---
+++ InterviewExam/Controller/InputConverter.py
@@ -0,0 +1,18 @@
+import json
+
+def convert_string(inp):
+    dict = json.loads(inp)
+    return dict
+
+def convert_by_type(inp,type):
+    if type==str:
+        dict = convert_string(inp)
+        print(dict)
+        return dict
+    print("Can't convert input to dictionary\json")
+    return {"message": "Can't convert input to dictionary"}
+
+def convert_to_dict(inp):
+    t = type(inp)
+    dict = convert_by_type(inp,t)
+    return dict
